255542c9032ad804a114f539f979e049980a9ddc
Blair Duddy <blairduddy@hotmail.co.uk>
2023-05-25 15:12:09 -0400

    Initial Commit

---
+++ Scramble/ScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrambleApp.swift
+//  Scramble
+//
+//  Created by Blair Duddy on 2023-05-25.
+//
+
+import SwiftUI
+
+@main
+struct ScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
